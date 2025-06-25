d72b08de462c4520cd4cc633f323cf48d40e4bc5
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-25 22:15:00 +0530

    Add files via upload

---
+++ pattern3.java
@@ -0,0 +1,29 @@
+package hello;
+
+public class pattern3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<5; i++) {
+			for(int j=5; j>i; j--) {
+				System.out.print(" ");
+			}
+			for(int k=0; k<=i; k++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+		
+		for(int i=1; i<5; i++){
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			 for(int k=4; k>=i; k--) {
+				 System.out.print("* ");
+			 }
+			 System.out.println();
+		}
+
+	}
+
+}
