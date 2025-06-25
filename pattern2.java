7e674bece8d664d5a1e9b999d30c5c0cc5b069b5
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-25 22:14:43 +0530

    Add files via upload

---
+++ pattern2.java
@@ -0,0 +1,18 @@
+package hello;
+
+public class pattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<5; i++){
+			for(int j=0; j<i; j++) {
+				System.out.print(" ");
+			}
+			 for(int k=i; k<5; k++ ) {
+				 System.out.print("* ");
+			 }
+			 System.out.println();
+		}
+	}
+
+}
