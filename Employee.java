a63f2045da4172afb78843cbd3289038012f6524
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-26 12:29:17 +0530

    Day5

--- Employee.java
+++ Employee.java
@@ -1,66 +1,46 @@
-package com.dem01;
+package com.demo2;
 import java.util.Scanner;
 
-public class Employee {
-	
+public class Employee extends Person {
 	int empid;
-	String name;
 	double salary;
-	Date doj;
-	Vehicle veh;
 	
 	public Employee() {
-		doj = new Date();
-	}
-	public Employee(int empid, String name, double salary, int day, int month, int year) {
-		this.empid = empid;
-		this.name = name;
-		this.salary = salary;
-		doj = new Date( day,  month,  year);
-	}
-	
-	public void acceptEmployee(Scanner sc) {
-		System.out.print("Enter the employee Id: ");
-		empid = sc.nextInt();
-		System.out.print("Enter the Employee Name: ");
-		name = sc.next();
-		System.out.print("Enter the Employee Salary: ");
-		salary = sc.nextDouble();
-		System.out.println("Enter the Date of joining: ");
-		doj.acceptDate(sc);
-		}
-	
-	public void addVehicle(Scanner sc) {
-		veh = new Vehicle();
-		veh.acceptVehicle(sc);
-	}
-	
-	public void displayEmployee() {
-		System.out.println("Employee Id: " + empid);
-		System.out.println("Employee's Name : "+ name);
-		System.out.println("Employee's Salary: "+ salary);
-		doj.displayDate();
-		if(veh != null) {
-			veh.displayVehicle();
-		}
+		System.out.println("Employee() ");
+		System.out.println();
 		
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Employee(int empid, String name, String email, double salary) {
+		super(name, email);
+		System.out.println("Employee(int,String,String,double)");
+		this.empid = empid;
+		this.salary = salary;
+	}
+	
+	public void acceptEmployee(Scanner sc) {
+		super.acceptPerson(sc);
+		System.out.println("Enter Employee Id:  ");
+		empid = sc.nextInt();
+		System.out.println("Enter Employee Salary: ");
+		salary = sc.nextDouble();
+	}
+	
+	public void displayEmployee() {
+		super.displayPerson();
+		System.out.println("Employee's Id: "+ empid);
+		System.out.println("Employee's Salary: "+salary);
+	}
+	
+	
+	public static void main (String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Employee e1 = new Employee();
+		Employee e1 = new Employee(1, "Siddhi","siddhi@gmail.com", 50000);
 		e1.displayEmployee();
 		
 		Employee e2 = new Employee();
 		e2.acceptEmployee(sc);
-		e2.addVehicle(sc);
 		e2.displayEmployee();
-		
-
 	}
-
-
-	
 
 }
