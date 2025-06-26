c4ee2272166bc5119c2c36fd38c393551349d569
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-26 12:25:31 +0530

    Day3

---
+++ program.java
@@ -0,0 +1,16 @@
+package package2;
+import package1.Time;
+
+
+public class program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Time t1 = new Time();
+		
+//		t1.accept();
+	t1.display();
+
+	}
+
+}
