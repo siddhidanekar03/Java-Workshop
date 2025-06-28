8f7560e81bdb87edf040a9b29207def95d3da768
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-06-28 19:20:28 +0530

    Add files via upload

--- Menu.java
+++ Menu.java
@@ -1,109 +1,122 @@
-package com.sunbeam;
+package com.app.dkte;
 import java.util.Scanner;
 
-public class Menu {
-	int rollno;
-	String name;
-	double marks;
-	
-	public Menu() {
-		
-	}
-	
-	public Menu(int rollno, String name, double marks) {
-		super();
-		this.rollno = rollno;
-		this.name = name;
-		this.marks = marks;
-	}
 
-	public static int menu(Scanner sc) {
-		System.out.println("-----------------");
-		System.out.println("0. EXIT");
-		System.out.println("1. Add Student");
-		System.out.println("2. Display Student");
-		System.out.println("3. Find Student");
-		System.out.print("Enter your choice:- ");
-		int choice = sc.nextInt();
-		System.out.println("-----------------");
-		return choice;
-	}
-	
-	public void accept(Scanner sc) {
-		System.out.print("Enter the Student Roll no. : ");
-		this.rollno = sc.nextInt();
-		sc.nextLine();
-		System.out.print("Enter the Student Name : ");
-		this.name = sc.nextLine();
-		System.out.print("Enter the Student Marks : ");
-		this.marks = sc.nextDouble();
-	}
-	public int getRollno() {
-		return this.rollno;
-	}
-	
-	
-	public void display() {
-		System.out.println("Roll no.: " + rollno);
-		System.out.println("Name : " + name);
-		System.out.println("Marks: " + marks);
-		System.out.println("--------");
-	}
-	
+public class Menu {
 
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		Menu[] arr = new Menu[5];
-		int index = 0;
+		Employee [] emp = new Employee[10];
 		int choice;
-		while ((choice = menu(sc)) != 0) {
-			switch (choice) {
-			case 1:
-				System.out.println("Add Student");
-				if (index < 5) {
-					arr[index] = new Menu();
-					arr[index].accept(sc);
-					index++;
-				} else
-					System.out.println("Course is full");
-				break;
-				
-			case 2:
-				System.out.println("Display Student Info");
-				for (Menu s : arr)
-					if (s != null)
-						s.display();
-				
-				break;
-				
-			case 3:
-				System.out.println("Find Student");
-				System.out.println("Enter Roll no. to search: ");
+		int index = 0;
+		
+		do {
+			System.out.println("0. Exit");
+			System.out.println("1. Add Employee");
+			System.out.println("2. Display Employee");
+			System.out.println("3. Search Employee by Id");
+			System.out.println("4. Display Employees joined in given Year:");
+			System.out.println("5. Find Employee with Maximum Salary:");
+			System.out.println("6. Find Employee with Minimum Salary");
+			System.out.println("Enter your Choice: ");
+			choice = sc.nextInt();
+		
+		
+		switch (choice) {
+		case 1: System.out.println("1. Add Employee");
+			if(index < 10) {
+				emp[index] = new Employee();
+				emp[index].acceptEmployee(sc);
+				index++;
+			}else {
+				System.out.println("Array is Full");
+			}
+			break;
+			
+		case 2: System.out.println("2. Display Employee");
+		         for(Employee e1 : emp) {
+		        	 if(e1 != null) {
+		        		 System.out.println(e1.toString());
+		        	 }
+		         }
+		         break;
+		         
+		case 3: System.out.println("3. Search Employee by Id");
+				System.out.println("Enter Employee Id to search");
 				int search = sc.nextInt();
 				boolean found = false;
-				for (Menu s : arr) {
-					if (s != null && s.getRollno() == search) {
-						System.out.println("Student Found");
-						s.display();
+				for(Employee e1 : emp) {
+					if(e1 != null && e1.getEmpid() == search) {
+						System.out.println("Employee Found");
+						e1.toString();
 						found = true;
 						break;
 					}
-				}
-					if(!found){
-						System.out.println("Not Found");
+					if(!found) {
+						System.out.println("Employee not found");
 					}
-					
-				
-			break;
-			
-			default:
-				System.out.println("Wrong choice..");
+				}
 				break;
-			}
+				
+		case 4: System.out.println("4. Display Employees joined in given Year:");
+			     System.out.println("Enter the year: ");
+			     int year = sc.nextInt();
+			     boolean match = false;
+			     for(Employee e1 : emp) {
+			    	 if(e1 != null && e1.getDateOfJoining().getYear() == year) {
+			    		 e1.toString();
+			    		 match = true;
+			    		 break;
+			    		 }
+			    	 if(!match) {
+			    		 System.out.println("Not found");
+			    	 }
+			     }
+			     break;
+			     
+		case 5: System.out.println("5. Find Employee with Maximum Salary:");
+		           double maxSalary = Double.MIN_VALUE;
+		           Employee maxEmp = null;
+		            for (Employee e1 : emp) {
+			        if (e1 != null && maxSalary < e1.getSalary()) {
+				     maxSalary = e1.getSalary();
+				     maxEmp = e1;
+			       }
+		     }
+		        if (maxEmp != null) {
+			        System.out.println("Employee with Maximum Salary: " + maxEmp);
+		        }
+		        else {
+			         System.out.println("Employee Not found");
+		        }
+		break;
+		
+		case 6: System.out.println("6. Find Employee with Minimum Salary");
+			    double minSalary = Double.MAX_VALUE;
+			    Employee minEmp = null;
+			    for(Employee e1 : emp) {
+			    	if(e1 != null && minSalary > e1.getSalary()) {
+			    		minSalary = e1.getSalary();
+			    		minEmp = e1;
+			    	}
+			    }
+			    
+			    if(minEmp != null) {
+			    	System.out.println("Employee with Minimum Salary: " + minEmp);
+			    }else {
+			    	System.out.println("Employee not found");
+			    }
+			    break;
+		case 0 : System.out.println("Exit");
+		break;
+		             
+		
+
+		default:System.out.println("Wrong Choice");
+			break;
 		}
-
+	}while(choice !=0 );
 	}
-
-	
 
 }
