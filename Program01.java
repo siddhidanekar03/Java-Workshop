8e24d774ae77e43587def7c0ca447351a129e573
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-01 12:06:37 +0530

    Day7 demo3

--- Program01.java
+++ Program01.java
@@ -1,21 +1,41 @@
-package com.demo4;
-import java.util.Scanner;
+package com.app.demo3;
 
+
+class Date{
+	int day;
+	int month;
+	public void setDay(int day) {
+		if(day <=0 || day > 30) 
+		throw new  RuntimeException();
+		this.day = day;
+	
+}
+	
+	public  void setMonth(int month) throws Exception {
+		if(month <= 0 || month > 12)
+	    throw new Exception();
+		this.month = month;
+}
+
+	@Override
+	public String toString() {
+		return "Date [day=" + day + ", month=" + month + "]";
+	}
+	
+	
+}
 public class Program01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Scanner sc = new Scanner(System.in);
-		
-		Employee e;//only reference
-		//e = new Employee();//not ok beacuse only reference of abstract class is allowed
-		//e = new Manager();//upcasting 
-		e = new Salesman();
-		e.accept(sc);
-		e.display();
-		e.calculateTotalSalary();
-	  
-
+		Date d1 = new Date();
+		d1.setDay(80);
+		try {
+			d1.setMonth(30);
+			System.out.println(d1);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
