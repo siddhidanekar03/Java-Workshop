d85f2e3baa86c2ff512c853b3daeaabd72a2d5a1
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-05 22:25:31 +0530

    Day10

--- Program01.java
+++ Program01.java
@@ -1,17 +1,31 @@
 package com.app.demo1;
 
+class Box <T>{
+	private T obj;
+	
+	public void setObj(T obj) {
+		this.obj = obj;
+	}
+	
+	public T getObj() {
+		
+		return obj;
+	}
+}
+
 public class Program01 {
 	
 	public static void main(String[] args) {
-		String s1 = "sunbeam";
-		//String s2 = new String("sunbeam");
-		String s2 = "sunbeam";
-
+		Box<Integer> b1 = new Box <Integer>();
+		b1.setObj(10);
 		
-		System.out.println("s1 - " + s1 );
-		System.out.println("s2 - " + s2);
-		System.out.println("s1==s2 " + (s1==s2));
+		Integer i1 = b1.getObj();
+		System.out.println("i1- " + i1);
 		
+		Box<String> b2 = new Box <String>();
+		b2.setObj("subeam");
+		String s1 = b2.getObj();
+		System.out.println("s1- " + s1);
 	}
 
 }
