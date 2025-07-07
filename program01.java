2e8b6c3eee307a2c7614ebaaacdde84ba372cc3f
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-07 12:32:01 +0530

    Day11 demo

--- program01.java
+++ program01.java
@@ -1,21 +1,40 @@
-package com.app.demo3;
-
-import java.util.StringTokenizer;
+package com.app.demo2;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.ArrayList;
 
 public class program01 {
-	
+
 	public static void main(String[] args) {
+		Collection<Integer> c1 = new ArrayList<>();
+		c1.add(10);
+		c1.add(20);
+		c1.add(30);
+		c1.add(40);
+		c1.add(50);
+		System.out.println("Array Size: " + c1.size());
+		System.out.println("Array contains 10: " + c1.contains(10));
 		
-		String s = "www.sunbeaminfotech.com";
-		System.out.println("OG String-> " + s);
-		System.out.println("Tokens - > ");
-		
-		StringTokenizer st = new StringTokenizer(s, ".");
-		while(st.hasMoreTokens()){
-			System.out.println(st.nextToken());
+//		for(Integer e : c1) {
+//			System.out.println(e);
+//		}
+//		//c1.clear();
+//		c1.remove(20);
+//		System.out.println("Display: ");
+//		
+//		for(Integer e : c1) {
+//			System.out.println(e);
+//		}
+//		
+		///Using iterator
+	     Iterator<Integer> itr  = c1.iterator();
+	    	 while(itr.hasNext()) {
+	    		 System.out.println(itr.next());
+	    	 }
+	     }
 			
-		}
 		
-	}
+
+	
 
 }
--- program02.java
+++ program02.java
@@ -1,21 +1,78 @@
-package com.app.demo3;
+package com.app.demo2;
 
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collection;
+
+
+class Employee{
+	int empid;
+	String Name;
+	double salary;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(int empid, String name, double salary) {
+		
+		this.empid = empid;
+		Name = name;
+		this.salary = salary;
+	}
+	
+	public Employee(int empid) {
+		this.empid = empid;
+	}
+
+	public boolean equals(Object obj) {
+		if(obj == null)
+			return false;
+		if(this == obj)
+			return true;
+		
+		if(obj instanceof Employee) {
+			Employee e = (Employee) obj;
+			if(this.empid == e.empid)
+				return true;
+		}
+		return false;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Employee [empid=" + empid + ", Name=" + Name + ", salary=" + salary + "]";
+	}
+	
+	
+	
+	
+}
 
 public class program02 {
 	
+	
+
 	public static void main(String[] args) {
 		
-		String s = "www sunbeaminfotech com";
-		System.out.println("OG String-> " + s);
-		System.out.println("Tokens - > ");
+		Collection<Employee> e = new ArrayList<>();
+		e.add(new Employee(1, "Anil", 20000));
+		e.add(new Employee(2, "Neha", 10000));
+		e.add(new Employee(3, "Siya", 40000));
+		e.add(new Employee(4, "Vansh", 30000));
+		e.add(new Employee(5, "Ansh", 50000));
+		e.add(new Employee(6, "Ram", 60000));
 		
-		StringTokenizer st = new StringTokenizer(s);
-		while(st.hasMoreTokens()){
-			System.out.println(st.nextToken());
-			
+		
+		System.out.println("Remove Employee: " + e.remove(new Employee(4)));
+		
+		System.out.println("Contains: " + e.contains(new Employee(3)));
+		
+		for(Employee emp : e ) {
+			System.out.println(emp);
 		}
-		
+	
+
 	}
 
 }
