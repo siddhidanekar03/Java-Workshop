5b39c655907d38a14dbb428d4d0f926c782886b4
siddhidanekar03 <danekarsiddhi@gmail.com>
2025-07-08 12:38:45 +0530

    Day13 demo03

--- program01.java
+++ program01.java
@@ -1,45 +1,74 @@
-package com.app.demo2;
-import java.util.Collection;
-import java.util.Map;
-import java.util.Set;
-import java.util.TreeMap;
+package com.app.demo3;
+import java.util.List;
+
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+interface Acceptable{
+	
+	void accept();//public abstract
+	
+	default void display() {
+		System.out.println("aceeptable :: display");
+	}
+	
+	static <T> void sort(List<T> l1) {
+		
+	}
+}
+
+//interface Displayable{
+//void display() ;
+//}
+   
+class Employee implements Acceptable{
+
+	@Override
+	public void accept() {
+		System.out.println("Employee :: Accept");
+		
+	}
+	@Override
+	public void display() {
+		System.out.println("Employee::Display");
+	}
+	
+}
+
+class Time implements Acceptable{
+
+	@Override
+	public void accept() {
+		System.out.println("Time :: Accept");
+		
+	}
+	
+	
+}
+
+class Product implements Acceptable{
+
+	@Override
+	public void accept() {
+		System.out.println("Product :: Accept");
+		
+	}
+	@Override
+	public void display() {
+		System.out.println("Product :: Display");
+	}
+	
+}
 
 public class program01 {
 
 	public static void main(String[] args) {
-	  Map<Integer, String> m = new TreeMap<>();
-	  m.put(111, "Neha");
-	  m.put(112, "Sanika");
-	  m.put(113,  "Yukta");
-	  m.put(114, "Revati");
-	  m.put(115, "Siya");
-	  m.put(115, "Siya");//duplicate keys are not allowed
-	//m.put(115, "Siya");//duplicate values are allowed
-	  m.put(115, "Riya");// If keys are duplicated it will replace the value
-	  m.put(117 ,null);
-	  m.put(118 ,null);
-	  
-	  
-	 
-	  
-	  System.out.println("Display Keys-> ");
-	  Set<Integer> keys = m.keySet();
-	  for(Integer k : keys) {
-		  System.out.println(k);
-	  }
-	  
-	  System.out.println("Display Values-> ");
-	 Collection<String> values = m.values();
-	  for(String v : values) {
-		  System.out.println(v);
-	  }
-	  
-	  Set<Map.Entry<Integer, String>> entries = m.entrySet();
-	  for(Map.Entry<Integer, String>e : entries ) {
-		  System.out.println(e.getKey() + "-" + e.getValue());
-	  }
-	  
-	  
+		Acceptable a;
+		//a = new Employee();
+		a = new Time();
+		//a = new Product();
+		
+	a.accept();
+	a.display();
 
 	}
 
